7d1ebb03d17421c8adf7a257104233d3b23d26b4
treytamaki <32915435+treytamaki@users.noreply.github.com>
2019-01-15 16:25:37 -0800

    deploy app

---
+++ build/precache-manifest.d7009eb16bc646b38a883f223b1c30d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "94393125069eb17ba4eaf8b55133b7d8",
+    "url": "/treytamaki.github.io/static/media/linkedin.94393125.png"
+  },
+  {
+    "revision": "25a93934a5b8a093f6de9f1bbb7000b1",
+    "url": "/treytamaki.github.io/static/media/emp.25a93934.jpg"
+  },
+  {
+    "revision": "1f45b0a0db2ac23407f63f7676e17ce9",
+    "url": "/treytamaki.github.io/static/media/Trey Tamaki Resume (December 2018).1f45b0a0.pdf"
+  },
+  {
+    "revision": "397b50a625f2e2ce6137",
+    "url": "/treytamaki.github.io/static/js/runtime~main.397b50a6.js"
+  },
+  {
+    "revision": "390a9e4d95512eac8ee5",
+    "url": "/treytamaki.github.io/static/js/main.390a9e4d.chunk.js"
+  },
+  {
+    "revision": "4b677be59e226e0d9d21",
+    "url": "/treytamaki.github.io/static/js/1.4b677be5.chunk.js"
+  },
+  {
+    "revision": "390a9e4d95512eac8ee5",
+    "url": "/treytamaki.github.io/static/css/main.bab93bde.chunk.css"
+  },
+  {
+    "revision": "c65380fa8073cfcdf9cbc884fec0ce82",
+    "url": "/treytamaki.github.io/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import MusicPage from './MusicPage.js';
+import Resume from './Resume.js';
+import Launchpad from './Launchpad.js';
+import Projects from './Projects.js';
+
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/music" component={MusicPage} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/launchpad" component={Launchpad} />
+        <Route path="/projects" component={Projects} />
+      </div>
+    </Router>
+  );
+
+
+ReactDOM.render(routing, document.getElementById('root'));
