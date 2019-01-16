3ae90f5c25928fad6e01ac97b6a34a70cb187ab9
treytamaki <32915435+treytamaki@users.noreply.github.com>
2019-01-16 00:37:41 -0800

    fix scripts again 4.0

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/treytamaki.github.io/precache-manifest.2b32815887864c395a0f8670cee17539.js"
+  "/website/precache-manifest.1830c99eaf46bbc65b2e5e5ee4978473.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/treytamaki.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
